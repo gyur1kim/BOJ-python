14f13db6e95afbcecfc1f0e5d92474ef052920e9
김규리 <qri1114@gmail.com>
2022-10-31 17:32:08 +0900

    221031_📆

---
+++ 그리디/11047_동전 0.py
@@ -0,0 +1,19 @@
+# N: 동전의 종류, K: 가격
+N, K = map(int, input().split())
+coins = []
+
+for i in range(N):
+    if (n := int(input())) < K:
+        coins.append(n)
+        continue
+    break
+
+coinNums = 0
+idx = -1
+while K:
+    coinNums += K // coins[idx]
+    K = K % coins[idx]
+    idx -= 1
+
+print(coinNums)
+exit()
