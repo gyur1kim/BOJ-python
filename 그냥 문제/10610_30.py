f6728cf2548d682ac8ab747fbe544fc8b00b6c99
김규리 <qri1114@gmail.com>
2022-11-01 10:10:52 +0900

    221101

--- 그냥 문제/10610_30.py
+++ 그냥 문제/10610_30.py
@@ -1,5 +1,19 @@
-n = list(map(int, input()))
+# n = list(map(int, input()))
+#
+# if sum(n) % 3:
+#     print(-1)
+# else:
+#     n = sorted(n, reverse=True)
+#     if n[-1]:
+#         print(-1)
+#     else:
+#         n = int(''.join(map(str, n)))
+#         print(n)
 
+
+## 숫자를 str로 바꾸는 데에는 O(logn)의 시간이 걸린다......
+## str로 안바꾸고 출력하면 어떨까
+n = list(map(int, input()))
 if sum(n) % 3:
     print(-1)
 else:
@@ -7,5 +21,13 @@ else:
     if n[-1]:
         print(-1)
     else:
-        n = int(''.join(map(str, n)))
-        print(n)
+        print(*n, sep="")
+
+
+## 이 방법이 제일 빠르다
+N_list = sorted(list(map(int, input())), reverse=True)
+print(-1) if N_list[-1] or sum(N_list) % 3 else print(*N_list, sep="")
+
+'''
+파이썬에서 print할 때 항상 end만 사용했는데 sep을 새롭게 배웠다.
+'''
